6ccdb98f561d11111c1a20be54c0167595c3c1be
jaysunxiao <sunquanxiao@corp.netease.com>
2025-05-22 15:54:02 +0800

    update

--- src/views/jiucai/dialog/Setting.vue
+++ src/views/jiucai/dialog/Setting.vue
@@ -23,32 +23,32 @@ const snackbarStore = useSnackbarStore();
       <v-card-text>
         <v-container>
           <v-row>
-            <v-col cols=1>
+            <v-col cols=2>
               <Icon width="30" icon="icon-park:broadcast-radio" />
             </v-col>
-            <v-col class="py-0" cols="6">
-              <v-switch v-model="myStore.newsNotify" :label="myStore.newsNotify ? '情报通知（许要打开浏览器通知）' : '情报通知（标题闪烁，桌面弹窗）'" hide-details color="teal" inset></v-switch>
+            <v-col class="py-0">
+              <v-switch v-model="myStore.newsNotify" label="情报通知" hide-details color="teal" inset></v-switch>
             </v-col>
           </v-row>
           <v-row>
-            <v-col cols=1>
+            <v-col cols=2>
               <Icon width="30" icon="icon-park:volume-notice" />
             </v-col>
-            <v-col class="py-0" cols="6">
-              <v-switch v-model="myStore.squareNotify" :label="myStore.squareNotify ? '聊天通知（许要打开浏览器通知）' : '聊天通知（标题闪烁，桌面弹窗）'" hide-details color="teal" inset></v-switch>
+            <v-col class="py-0">
+              <v-switch v-model="myStore.squareNotify" label="聊天通知" hide-details color="teal" inset></v-switch>
             </v-col>
           </v-row>
           <v-row>
-            <v-col cols=1>
+            <v-col cols=2>
               <Icon width="30" icon="fluent-emoji-flat:party-popper" />
             </v-col>
-            <v-col class="py-0" cols="6">
+            <v-col class="py-0">
               <v-switch v-model="myStore.particles" label="粒子特效" hide-details color="teal" inset></v-switch>
             </v-col>
           </v-row>
           <v-row>
             <v-col>
-              按照下图的方式打开浏览器通知权限
+              Attention: 情报和聊天通知需要按照下图的方式打开浏览器通知权限
               <v-img src="aa/image/browser-notify.png"/>
             </v-col>
           </v-row>
